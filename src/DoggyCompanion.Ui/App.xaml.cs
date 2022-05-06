c25be1786a111fc1299f92d350dbfeba914355af
Ethan Guest <eguest01@gmail.com>
2022-05-05 21:34:38 -0500

    initial solution and project

---
+++ src/DoggyCompanion.Ui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DoggyCompanion.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/DoggyCompanion.Ui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DoggyCompanion.Ui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
